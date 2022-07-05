e830b282d786616cccbcff85e002e6de47a948ad
Brayan Martinez <brayanmartinezojeda0@gmail.com>
2022-07-04 23:36:09 -0500

    changes heroku server1

--- app/Http/Livewire/Admin/ReportComponent.php
+++ app/Http/Livewire/Admin/ReportComponent.php
@@ -12,7 +12,7 @@ class ReportComponent extends Component
     use WithPagination;
     public $fecha_inicio, $fecha_fin;
     public $sumtotal;
-    
+
     public function eliminarfiltro(){
         $this->fecha_inicio ='';
         $this->fecha_fin ='';
@@ -22,20 +22,16 @@ class ReportComponent extends Component
         if (!$this->fecha_inicio == '' && !$this->fecha_fin == '') {
             $fi = Carbon::parse($this->fecha_inicio)->format('Y-m-d 00:00:00');
             $ff = Carbon::parse($this->fecha_fin)->format('Y-m-d 23:59:59');
-            $orders = Order::where('status','<>', 1)
-                        ->where('status','<>', 5)
+            $orders = Order::where('status','=', 4)
                         ->wherebetween('created_at', [$fi, $ff])
                         ->paginate(10);
             $this->sumtotal = Order::whereBetween('created_at', [$fi, $ff])
-                        ->where('status','<>', 5)
-                        ->where('status','<>', 1)
+                        ->where('status','=', 4)
                         ->sum('total');
         }else{
-            $orders = Order::where('status','<>', 1)
-                        ->where('status','<>', 5)
+            $orders = Order::where('status','=', 4)
                         ->paginate(10);
-            $this->sumtotal = Order::where('status','<>', 5)
-                            ->where('status','<>', 1)
+            $this->sumtotal = Order::where('status','=', 4)
                             ->sum('total');
         }
         $sumtotal =$this->sumtotal;
