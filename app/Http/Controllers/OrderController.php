7f5838c2bf7dfd8dd95f8163b01ec724c2d7d9e9
Brayan Martinez <brayanmartinezojeda0@gmail.com>
2022-07-05 13:42:57 -0500

    changes heroku server2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     public function index(){
-        $orders = Order::query()->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 day', '+19 Hours')->format('Y-m-d 23:59:59')]);
+        $orders = Order::query()->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 23:59:59')]);
 
 
         if (request('status')) {
@@ -16,11 +16,11 @@ class OrderController extends Controller
 
         $orders=$orders->get();
 
-        $pendiente = Order::where('status', 1)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
-        $preparando = Order::where('status', 2)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
-        $enviado = Order::where('status', 3)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
-        $pagado = Order::where('status', 4)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
-        $anulado = Order::where('status', 5)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
+        $pendiente = Order::where('status', 1)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
+        $preparando = Order::where('status', 2)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
+        $enviado = Order::where('status', 3)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
+        $pagado = Order::where('status', 4)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
+        $anulado = Order::where('status', 5)->where('user_id', auth()->user()->id)->whereBetween('created_at', [now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 00:00:00'), now()->parse('-1 Day', '+19 Hours')->format('Y-m-d 23:59:59')])->count();
 
         return view('orders.index', compact('orders', 'pendiente', 'preparando', 'enviado', 'pagado', 'anulado'));
     }
