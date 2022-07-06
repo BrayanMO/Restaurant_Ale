3907bb9ade027cebd90bc95c618a4d7d12e52e88
Brayan Martinez <brayanmartinezojeda0@gmail.com>
2022-07-05 19:39:53 -0500

    changes heroku server5.13

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('orders/{order}/{mesa}', [OrderCreate::class, 'show'])->name('orders.
 
 Route::get('pedido', [PedidoController::class, 'index'])->name('pedido.index');
 Route::get('pedido/{order}', [PedidoController::class, 'show'])->name('pedido.show');
-Route::get('registrar', [Loginer::class, 'index'])->name('registrar');
+Route::get('registrar', function(){return view('auth.register');})->name('registrar');
 
 });
 
